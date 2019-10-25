e3ff87ef12db89652c88b45ef516dc0da0122d4d
Thibault-GX <thibault.grimoux@gmail.com>
2019-10-25 09:20:55 +0200

    Push initial

---
+++ javascript.js
@@ -0,0 +1,7 @@
+function controle(){
+let firstname = document.getElementById("firstname").value;
+let lastname = document.getElementById("lastname").value;
+let city = document.getElementById("city").value;
+
+alert(`Bonjour, ${firstname} ${lastname}, il paraît que vous habitez sur ${city}. Labès ?`);
+}
